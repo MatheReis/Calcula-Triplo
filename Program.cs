9344d21363498906c6e25b81470213b17de34417
Matheus Reis <83931417+MatheReis@users.noreply.github.com>
2021-08-22 09:22:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CShrap_Calcula_Triplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //declaração das variáveis do programa
+           double Num, Triplo;
+
+           //comando "LimpaTela" limpa a tela do monitor de vídeo
+           Console.Clear();
+
+            // {Entrada dos dados}
+            //mensagem para que o usuári saiba o que deve fazer
+            Console.Write("Digite um número: ");
+
+           //leitura do número para a memória
+           Num = double.Parse(Console.ReadLine());
+
+           //{Processamento}
+           //calcula o triplo e armazena o resultado na variável Triplo
+           Triplo = Num * 3;
+
+           // {Saída da informação}
+           //apenas salta uma linha na tela do monitor de vídeo
+           Console.WriteLine();
+
+           //exibe o resultado
+            Console.WriteLine($"O Triplo de {Num}  é  {Triplo}");
+        }
+    }
+}
